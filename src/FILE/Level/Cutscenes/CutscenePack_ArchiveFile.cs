ee742531f5cae5322d9157100c5e8372a24c9aee
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-07-17 22:00:15 +0200

    Rename FontImgData -> CharacterNamesImgData

--- src/FILE/Level/Cutscenes/CutscenePack_ArchiveFile.cs
+++ src/FILE/Level/Cutscenes/CutscenePack_ArchiveFile.cs
@@ -5,7 +5,7 @@
         public SpriteAnimations_File SpriteAnimations { get; set; }
         public Sprites_ArchiveFile Sprites { get; set; } // TODO: What is this?
         public ArchiveFile<CutsceneRawTextureData> File_2 { get; set; } // TODO: What is this? Seems to be Klonoa frames (and other characters?)
-        public RawData_File FontImgData { get; set; } // Raw image data, gets loaded at (0x3f4, 0x180, 0xc, 0x80)
+        public RawData_File CharacterNamesImgData { get; set; } // Raw image data, gets loaded at (0x3f4, 0x180, 0xc, 0x50)
         public RawData_ArchiveFile File_4 { get; set; } // TODO: Parse
         public Cutscene[] Cutscenes { get; set; }
 
@@ -14,7 +14,7 @@
             SpriteAnimations = SerializeFile<SpriteAnimations_File>(s, SpriteAnimations, 0, name: nameof(SpriteAnimations));
             Sprites = SerializeFile<Sprites_ArchiveFile>(s, Sprites, 1, name: nameof(Sprites));
             File_2 = SerializeFile<ArchiveFile<CutsceneRawTextureData>>(s, File_2, 2, name: nameof(File_2));
-            FontImgData = SerializeFile<RawData_File>(s, FontImgData, 3, name: nameof(FontImgData));
+            CharacterNamesImgData = SerializeFile<RawData_File>(s, CharacterNamesImgData, 3, name: nameof(CharacterNamesImgData));
             File_4 = SerializeFile<RawData_ArchiveFile>(s, File_4, 4, name: nameof(File_4));
 
             var count = (OffsetTable.FilesCount - 5) / 3;
