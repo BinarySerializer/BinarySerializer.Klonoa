4ee2fe6efe4c29660ca8c8490274245aa1c529b5
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-07-18 21:18:56 +0200

    Parse more 3D objects

--- src/CODE/Object3D/Object3D.cs
+++ src/CODE/Object3D/Object3D.cs
@@ -26,6 +26,11 @@ namespace BinarySerializer.KlonoaDTP
         public Object3DPosition_File Data_Position { get; set; } // TODO: Objects which move have multiple positions
         public Object3DTransform_ArchiveFile Data_Transform { get; set; }
         public PS1_TIM Data_TIM { get; set; }
+        public TIM_ArchiveFile Data_TIMFiles { get; set; }
+        
+        public Object3DType3Data_File Data_Type3 { get; set; }
+        public Object3DType7Data_File Data_Type7 { get; set; }
+        public RawData_File Data_Type21 { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
@@ -46,13 +51,21 @@ namespace BinarySerializer.KlonoaDTP
             if (Type ==  Object3DType.Invalid || Type == Object3DType.None)
                 return;
 
+            if (Pointer_14 == null)
+            {
+                s.LogWarning($"Object3D of type {Type} has no data");
+                return;
+            }
+
+            var dataPack = Pre_ObjectModelsDataPack;
+
             // In Vision 1-1 NTSC the function pointer table is at 0x80110808, with a length of 24
             switch (Type)
             {
                 case Object3DType.Type_1:
-                    Data_TMD = Pre_ObjectModelsDataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
+                    Data_TMD = dataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
                     // TODO: File 1 seems to usually be another TMD, but not when Short_08 is 40 - why? What is it then?
-                    Data_Position = Pre_ObjectModelsDataPack.SerializeFile<Object3DPosition_File>(s, Data_Position, DataFileIndices[2], name: nameof(Data_Position));
+                    Data_Position = dataPack.SerializeFile<Object3DPosition_File>(s, Data_Position, DataFileIndices[2], name: nameof(Data_Position));
                     
                     if (DataFileIndices[3] != 0)
                         s.LogWarning($"Object3D of type {Type} has additional referenced files");
@@ -60,12 +73,17 @@ namespace BinarySerializer.KlonoaDTP
                     break;
 
                 case Object3DType.Type_3:
-                    // Unknown (array of ints which seem to be offsets, terminated with -1)
+                    Data_Type3 = dataPack.SerializeFile<Object3DType3Data_File>(s, Data_Type3, DataFileIndices[0], name: nameof(Data_Type3));
+
+                    if (DataFileIndices[1] != 0)
+                        s.LogWarning($"Object3D of type {Type} has additional referenced files");
+
                     break;
 
                 case Object3DType.Type_5:
-                    Data_TMD = Pre_ObjectModelsDataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
-                    Data_Transform = Pre_ObjectModelsDataPack.SerializeFile<Object3DTransform_ArchiveFile>(s, Data_Transform, DataFileIndices[1], name: nameof(Data_Transform));
+                case Object3DType.Type_8:
+                    Data_TMD = dataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
+                    Data_Transform = dataPack.SerializeFile<Object3DTransform_ArchiveFile>(s, Data_Transform, DataFileIndices[1], name: nameof(Data_Transform));
 
                     if (DataFileIndices[2] != 0)
                         s.LogWarning($"Object3D of type {Type} has additional referenced files");
@@ -73,11 +91,37 @@ namespace BinarySerializer.KlonoaDTP
                     break;
 
                 case Object3DType.Type_6:
-                    Data_TMD = Pre_ObjectModelsDataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
-                    Data_Transform = Pre_ObjectModelsDataPack.SerializeFile<Object3DTransform_ArchiveFile>(s, Data_Transform, DataFileIndices[1], name: nameof(Data_Transform));
-                    Data_TIM = Pre_ObjectModelsDataPack.SerializeFile<PS1_TIM>(s, Data_TIM, DataFileIndices[2], name: nameof(Data_TIM));
+                    Data_TMD = dataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
+                    Data_Transform = dataPack.SerializeFile<Object3DTransform_ArchiveFile>(s, Data_Transform, DataFileIndices[1], name: nameof(Data_Transform));
+                    Data_TIM = dataPack.SerializeFile<PS1_TIM>(s, Data_TIM, DataFileIndices[2], name: nameof(Data_TIM));
 
                     if (DataFileIndices[3] != 0)
+                        s.LogWarning($"Object3D of type {Type} has additional referenced files");
+
+                    break;
+
+                case Object3DType.Type_7:
+                case Object3DType.Type_9:
+                    Data_Type7 = dataPack.SerializeFile<Object3DType7Data_File>(s, Data_Type7, DataFileIndices[0], name: nameof(Data_Type7));
+
+                    if (DataFileIndices[1] != 0)
+                        s.LogWarning($"Object3D of type {Type} has additional referenced files");
+
+                    break;
+
+                case Object3DType.Type_10:
+                    Data_TIMFiles = dataPack.SerializeFile<TIM_ArchiveFile>(s, Data_TIMFiles, DataFileIndices[0], name: nameof(Data_TIMFiles));
+
+                    if (DataFileIndices[1] != 0)
+                        s.LogWarning($"Object3D of type {Type} has additional referenced files");
+
+                    break;
+
+                case Object3DType.Type_21:
+                    Data_TMD = dataPack.SerializeFile<PS1_TMD>(s, Data_TMD, DataFileIndices[0], logIfNotFullyParsed: false, name: nameof(Data_TMD));
+                    Data_Type21 = dataPack.SerializeFile<RawData_File>(s, Data_Type21, DataFileIndices[1], name: nameof(Data_Type21));
+
+                    if (DataFileIndices[2] != 0)
                         s.LogWarning($"Object3D of type {Type} has additional referenced files");
 
                     break;
@@ -92,12 +136,56 @@ namespace BinarySerializer.KlonoaDTP
         {
             Invalid = -1,
             None = 0,
-            Type_1 = 1, // Light source?
+            Type_1 = 1,
 
             Type_3 = 3,
 
             Type_5 = 5,
             Type_6 = 6,
+            Type_7 = 7,
+            Type_8 = 8,
+            Type_9 = 9,
+            Type_10 = 10,
+
+            Type_21 = 21,
+        }
+
+        // TODO: Name and move to files
+        public class Object3DType3Data_File : BaseFile
+        {
+            public int[] Offsets { get; set; }
+
+            public override void SerializeImpl(SerializerObject s)
+            {
+                Offsets = s.SerializeArrayUntil(Offsets, x => x == -1, () => -1, name: nameof(Offsets));
+            }
+        }
+        public class Object3DType7Data_File : BaseFile
+        {
+            public short EntriesCount { get; set; }
+            public short Short_02 { get; set; }
+            public Entry[] Entries { get; set; }
+
+            public override void SerializeImpl(SerializerObject s)
+            {
+                EntriesCount = s.Serialize<short>(EntriesCount, name: nameof(EntriesCount));
+                Short_02 = s.Serialize<short>(Short_02, name: nameof(Short_02));
+                Entries = s.SerializeObjectArray<Entry>(Entries, EntriesCount, name: nameof(Entries));
+            }
+
+            public class Entry : BinarySerializable
+            {
+                public short Short_00 { get; set; }
+                public short Short_02 { get; set; }
+                public short Short_04 { get; set; }
+
+                public override void SerializeImpl(SerializerObject s)
+                {
+                    Short_00 = s.Serialize<short>(Short_00, name: nameof(Short_00));
+                    Short_02 = s.Serialize<short>(Short_02, name: nameof(Short_02));
+                    Short_04 = s.Serialize<short>(Short_04, name: nameof(Short_04));
+                }
+            }
         }
     }
 }
