50e65bf2538feb85dbd395a18291e35ce10ddedb
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-08-03 10:11:03 +0200

    Parse portal obj spawn objects

--- src/CODE/LevelData2D/Enemy/Data/BaseEnemyData.cs
+++ src/CODE/LevelData2D/Enemy/Data/BaseEnemyData.cs
@@ -2,6 +2,7 @@
 {
     public abstract class BaseEnemyData : BinarySerializable
     {
+        public EnemyObject Pre_EnemyObj { get; set; }
         public Pointer[] Pre_DataPointers { get; set; }
 
         public int DespawnDistance { get; set; } = -1; // The distance from Klonoa before despawning
