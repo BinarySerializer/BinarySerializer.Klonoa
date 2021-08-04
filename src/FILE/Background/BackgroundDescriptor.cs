f90d157e09a026d4217b2189043ab8fd9e0e83d3
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-08-04 10:39:11 +0200

    Correctly parse background descriptors

---
+++ src/FILE/Background/BackgroundDescriptor.cs
@@ -0,0 +1,35 @@
+﻿namespace BinarySerializer.KlonoaDTP
+{
+    public class BackgroundDescriptor : BinarySerializable
+    {
+        public short XPos { get; set; }
+        public short YPos { get; set; }
+        public short Type { get; set; }
+
+        public int BGDIndex { get; set; }
+        public int UnknownValues { get; set; }
+        public int CELIndex { get; set; }
+        public bool UnknownFlag1 { get; set; }
+        public bool UnknownFlag2 { get; set; }
+
+        public byte[] Data { get; set; } // This data is structured differently depending on the type
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            XPos = s.Serialize<short>(XPos, name: nameof(XPos));
+            YPos = s.Serialize<short>(YPos, name: nameof(YPos));
+            Type = s.Serialize<short>(Type, name: nameof(Type));
+
+            s.SerializeBitValues<ushort>(bitFunc =>
+            {
+                BGDIndex = bitFunc(BGDIndex, 5, name: nameof(BGDIndex));
+                UnknownValues = bitFunc(UnknownValues, 7, name: nameof(UnknownValues));
+                CELIndex = bitFunc(CELIndex, 2, name: nameof(CELIndex));
+                UnknownFlag1 = bitFunc(UnknownFlag1 ? 1 : 0, 1, name: nameof(UnknownFlag1)) == 1;
+                UnknownFlag2 = bitFunc(UnknownFlag2 ? 1 : 0, 1, name: nameof(UnknownFlag2)) == 1;
+            });
+
+            Data = s.SerializeArray<byte>(Data, 56, name: nameof(Data));
+        }
+    }
+}
--- src/FILE/Background/BackgroundPack_ArchiveFile.cs
+++ src/FILE/Background/BackgroundPack_ArchiveFile.cs
@@ -10,14 +10,14 @@ namespace BinarySerializer.KlonoaDTP
         public ArchiveFile<PS1_TIM> TIMFiles { get; set; } // Tilesets
         public ArchiveFile<PS1_CEL> CELFiles { get; set; } // Tiles
         public ArchiveFile<PS1_BGD> BGDFiles { get; set; } // Maps
-        public ArchiveFile<TileAnimations_File> TileAnimationFiles { get; set; }
+        public ArchiveFile<BackgroundDescriptors_File> BackgroundDescriptorsFiles { get; set; } // One for each sector
 
         protected override void SerializeFiles(SerializerObject s)
         {
             TIMFiles = SerializeFile<ArchiveFile<PS1_TIM>>(s, TIMFiles, 0, name: nameof(TIMFiles));
             CELFiles = SerializeFile<ArchiveFile<PS1_CEL>>(s, CELFiles, 1, name: nameof(CELFiles));
             BGDFiles = SerializeFile<ArchiveFile<PS1_BGD>>(s, BGDFiles, 2, name: nameof(BGDFiles));
-            TileAnimationFiles = SerializeFile<ArchiveFile<TileAnimations_File>>(s, TileAnimationFiles, 3, name: nameof(TileAnimationFiles));
+            BackgroundDescriptorsFiles = SerializeFile<ArchiveFile<BackgroundDescriptors_File>>(s, BackgroundDescriptorsFiles, 3, name: nameof(BackgroundDescriptorsFiles));
         }
     }
 }
