e0a865ceb80eacc4c92b2b1c141dab18dbe174b1
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-10-08 17:50:41 +0200

    KH: Parse the font

--- src/KlonoaHeroes/KlonoaHeroesROM.cs
+++ src/KlonoaHeroes/KlonoaHeroesROM.cs
@@ -4,6 +4,8 @@ namespace BinarySerializer.Klonoa.KH
 {
     public class KlonoaHeroesROM : GBA_ROMBase
     {
+        public Graphics_File Font { get; set; }
+
         public StoryPack_ArchiveFile StoryPack { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
@@ -12,6 +14,7 @@ namespace BinarySerializer.Klonoa.KH
 
             // TODO: Move pointers to pointer table to avoid hard-coding them
 
+            s.DoAt(new Pointer(0x0829666c, Offset.File), () => Font = s.SerializeObject<Graphics_File>(Font, name: nameof(Font)));
             s.DoAt(new Pointer(0x0873d350, Offset.File), () => StoryPack = s.SerializeObject<StoryPack_ArchiveFile>(StoryPack, name: nameof(StoryPack)));
         }
     }
