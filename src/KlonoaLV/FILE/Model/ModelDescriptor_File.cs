854f981eec14d4a39c18edd8763903b5ccf83f09
Matthew Finerty <evilarceus@gmail.com>
2021-10-11 19:35:51 -0700

    LV: Parse model name

--- src/KlonoaLV/FILE/Model/ModelDescriptor_File.cs
+++ src/KlonoaLV/FILE/Model/ModelDescriptor_File.cs
@@ -1,5 +1,10 @@
 namespace BinarySerializer.Klonoa.LV {
     public class ModelDescriptor_File : BaseFile {
-        public override void SerializeImpl(SerializerObject s) { }
+        public string ModelName { get; set; }
+        
+        public override void SerializeImpl(SerializerObject s) 
+        {
+            ModelName = s.SerializeString(ModelName, 3, name: nameof(ModelName));
+        }
     }
 }
