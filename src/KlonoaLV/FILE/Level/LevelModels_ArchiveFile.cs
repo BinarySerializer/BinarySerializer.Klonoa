517438f0df728d725d45433ae388939817d15b76
Matthew Finerty <evilarceus@gmail.com>
2021-10-14 03:14:12 -0700

    LV: Revert serialization check for models and animations

--- src/KlonoaLV/FILE/Level/LevelModels_ArchiveFile.cs
+++ src/KlonoaLV/FILE/Level/LevelModels_ArchiveFile.cs
@@ -1,23 +1,20 @@
 using System.Collections.Generic;
 
-namespace BinarySerializer.Klonoa.LV {
-    public class LevelModels_ArchiveFile : ArchiveFile {
+namespace BinarySerializer.Klonoa.LV
+{
+    public class LevelModels_ArchiveFile : ArchiveFile
+    {
         public ArchiveFile<Model_ArchiveFile>[] Models;
         public RawData_File File_Last { get; set; } // ?
 
         protected override void SerializeFiles(SerializerObject s)
         {   
             Models = new ArchiveFile<Model_ArchiveFile>[OffsetTable.FilesCount - 1];
-            List<long> serializedOffsets = new List<long>();
-            serializedOffsets.Add(OffsetTable.FilePointers[OffsetTable.FilesCount - 1].FileOffset); // Don't parse the last file as a model
             for (int i = 0; i < OffsetTable.FilesCount - 1; i++)
             {
-                // Don't serialize models that have already been serialized
-                if (serializedOffsets.Contains(OffsetTable.FilePointers[i].FileOffset))
-                    continue;
-                
+                if (OffsetTable.FilePointers[i].FileOffset == OffsetTable.FilePointers[OffsetTable.FilesCount - 1].FileOffset)
+                    break; // Don't parse the last file
                 Models[i] = SerializeFile<ArchiveFile<Model_ArchiveFile>>(s, Models[i], i, name: $"{nameof(Models)}[{i}]");
-                serializedOffsets.Add(OffsetTable.FilePointers[i].FileOffset);
             }
 
             File_Last = SerializeFile(s, File_Last, OffsetTable.FilesCount - 1, name: nameof(File_Last));
--- src/KlonoaLV/FILE/Model/ModelAnimData_ArchiveFile.cs
+++ src/KlonoaLV/FILE/Model/ModelAnimData_ArchiveFile.cs
@@ -1,7 +1,7 @@
-using System.Collections.Generic;
-
-namespace BinarySerializer.Klonoa.LV {
-    public class ModelAnimData_ArchiveFile : ArchiveFile {
+namespace BinarySerializer.Klonoa.LV 
+{
+    public class ModelAnimData_ArchiveFile : ArchiveFile
+    {
         public ModelBoneData_File ModelBoneData { get; set; }
         public ModelAnimation_File[] ModelAnimations { get; set; }
 
@@ -10,20 +10,10 @@ namespace BinarySerializer.Klonoa.LV {
             if (OffsetTable.FilesCount != 0)
             {
                 ModelBoneData = SerializeFile(s, ModelBoneData, 0, name: nameof(ModelBoneData));
-
                 ModelAnimations = new ModelAnimation_File[OffsetTable.FilesCount - 1];
-                List<long> serializedOffsets = new List<long>();
                 for (int i = 1; i < OffsetTable.FilesCount; i++)
-                {
-                    // Don't serialize animations that have already been serialized
-                    if (serializedOffsets.Contains(OffsetTable.FilePointers[i].FileOffset))
-                        continue;
-                    
                     ModelAnimations[i - 1] = SerializeFile(s, ModelAnimations[i - 1], i, name: $"{nameof(ModelAnimations)}[{i}]");
-                    serializedOffsets.Add(OffsetTable.FilePointers[i].FileOffset);
-                }
             }
-            
         }
     }
 }
