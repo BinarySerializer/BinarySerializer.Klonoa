60414f866a629d82c99cd7073edde91d5d1fb85b
Matthew Finerty <evilarceus@gmail.com>
2021-10-17 01:11:17 -0700

    LV: Use IsEmpty property for empty morph targets

--- src/KlonoaLV/FILE/Model/ModelMorphTarget_File.cs
+++ src/KlonoaLV/FILE/Model/ModelMorphTarget_File.cs
@@ -4,6 +4,7 @@ namespace BinarySerializer.Klonoa.LV {
         // This is needed to get the vertices/normals pointers from the skin descriptors of the target mesh
         public ModelMesh[] Pre_Meshes { get; set; }
 
+        public bool IsEmpty { get; set; }
         public string Magic { get; set; } // FZ
         public byte[] Magic2 { get; set; } // 80 00 40 00
         public ushort MeshCount { get; set; } // Should always be 1
@@ -15,35 +16,40 @@ namespace BinarySerializer.Klonoa.LV {
         public Pointer VerticesPointer { get; set; }
         public Pointer NormalsPointer { get; set; }
         
-        // Separated by skin descriptors
+        // Separated by skin descriptor
         public KlonoaLV_Vector16[][] Vertices { get; set; }
         public KlonoaLV_Vector16[][] Normals { get; set; }
 
         public override void SerializeImpl(SerializerObject s) 
         {
-            Magic = s.SerializeString(Magic, 2, name: nameof(Magic));
-            MeshCount = s.Serialize<ushort>(MeshCount, name: nameof(MeshCount));
-            Magic2 = s.SerializeArray<byte>(Magic2, 4, name: nameof(Magic2));
-            VertexScale = s.Serialize<float>(VertexScale, name: nameof(VertexScale));
-            s.SerializePadding(4, logIfNotNull: true);
-            TargetMesh = s.Serialize<ushort>(TargetMesh, name: nameof(TargetMesh));
-            DataSize = s.Serialize<ushort>(DataSize, name: nameof(DataSize));
-            VertexCount = s.Serialize<ushort>(VertexCount, name: nameof(VertexCount));
-            NormalCount = s.Serialize<ushort>(NormalCount, name: nameof(NormalCount));
-            VerticesPointer = s.SerializePointer(VerticesPointer, anchor: Offset, name: nameof(VerticesPointer));
-            NormalsPointer = s.SerializePointer(NormalsPointer, anchor: Offset, name: nameof(NormalsPointer));
-
-            ModelMesh mesh = Pre_Meshes[TargetMesh];
-            Vertices ??= new KlonoaLV_Vector16[mesh.SkinDescriptorCount][];
-            Normals ??= new KlonoaLV_Vector16[mesh.SkinDescriptorCount][];
-
-            for (int i = 0; i < mesh.SkinDescriptorCount; i++)
+            IsEmpty = Pre_FileSize == 16;
+            
+            if (!IsEmpty)
             {
-                ModelSkinDescriptor skinDescriptor = mesh.SkinDescriptors[i];
-                Pointer verticesPointer = new Pointer(skinDescriptor.VerticesPointer.FileOffset - skinDescriptor.Pre_VerticesPointer.FileOffset + VerticesPointer.FileOffset, Offset.File);
-                Pointer normalsPointer = new Pointer(skinDescriptor.NormalsPointer.FileOffset - skinDescriptor.Pre_NormalsPointer.FileOffset + NormalsPointer.FileOffset, Offset.File);
-                s.DoAt(verticesPointer, () => Vertices[i] = s.SerializeObjectArray<KlonoaLV_Vector16>(Vertices[i], skinDescriptor.VertexCount, name: $"{nameof(Vertices)}[{i}]"));
-                s.DoAt(normalsPointer, () => Normals[i] = s.SerializeObjectArray<KlonoaLV_Vector16>(Normals[i], skinDescriptor.NormalCount, name: $"{nameof(Normals)}[{i}]"));
+                Magic = s.SerializeString(Magic, 2, name: nameof(Magic));
+                MeshCount = s.Serialize<ushort>(MeshCount, name: nameof(MeshCount));
+                Magic2 = s.SerializeArray<byte>(Magic2, 4, name: nameof(Magic2));
+                VertexScale = s.Serialize<float>(VertexScale, name: nameof(VertexScale));
+                s.SerializePadding(4, logIfNotNull: true);
+                TargetMesh = s.Serialize<ushort>(TargetMesh, name: nameof(TargetMesh));
+                DataSize = s.Serialize<ushort>(DataSize, name: nameof(DataSize));
+                VertexCount = s.Serialize<ushort>(VertexCount, name: nameof(VertexCount));
+                NormalCount = s.Serialize<ushort>(NormalCount, name: nameof(NormalCount));
+                VerticesPointer = s.SerializePointer(VerticesPointer, anchor: Offset, name: nameof(VerticesPointer));
+                NormalsPointer = s.SerializePointer(NormalsPointer, anchor: Offset, name: nameof(NormalsPointer));
+
+                ModelMesh mesh = Pre_Meshes[TargetMesh];
+                Vertices ??= new KlonoaLV_Vector16[mesh.SkinDescriptorCount][];
+                Normals ??= new KlonoaLV_Vector16[mesh.SkinDescriptorCount][];
+
+                for (int i = 0; i < mesh.SkinDescriptorCount; i++)
+                {
+                    ModelSkinDescriptor skinDescriptor = mesh.SkinDescriptors[i];
+                    Pointer verticesPointer = new Pointer(skinDescriptor.VerticesPointer.FileOffset - skinDescriptor.Pre_VerticesPointer.FileOffset + VerticesPointer.FileOffset, Offset.File);
+                    Pointer normalsPointer = new Pointer(skinDescriptor.NormalsPointer.FileOffset - skinDescriptor.Pre_NormalsPointer.FileOffset + NormalsPointer.FileOffset, Offset.File);
+                    s.DoAt(verticesPointer, () => Vertices[i] = s.SerializeObjectArray<KlonoaLV_Vector16>(Vertices[i], skinDescriptor.VertexCount, name: $"{nameof(Vertices)}[{i}]"));
+                    s.DoAt(normalsPointer, () => Normals[i] = s.SerializeObjectArray<KlonoaLV_Vector16>(Normals[i], skinDescriptor.NormalCount, name: $"{nameof(Normals)}[{i}]"));
+                }
             }
         }
     }
