16ca0ceebcd4c5a09e23c3dae24e2c5c1896e04f
Matthew Finerty <evilarceus@gmail.com>
2021-10-20 00:10:54 -0700

    LV: Fix parsing menu sprite textures

--- src/KlonoaLV/FILE/Texture/GSTextures_File.cs
+++ src/KlonoaLV/FILE/Texture/GSTextures_File.cs
@@ -16,6 +16,12 @@ namespace BinarySerializer.Klonoa.LV
             DMATag = s.SerializeObject<Chain_DMAtag>(DMATag, name: nameof(DMATag));
             if (DMATag.QWC != 0)
             {
+                if (DMATag.ADDR == 0x10 && DMATag.QWC == 0x01)
+                {
+                    // Menu sprites have an extra DMAtag for whatever reason
+                    s.Align(16);
+                    DMATag = s.SerializeObject<Chain_DMAtag>(DMATag, name: nameof(DMATag));
+                }
                 VIFCode_NOP = s.SerializeObject<VIFcode>(VIFCode_NOP, name: nameof(VIFCode_NOP));
                 VIFCode_DIRECTHL = s.SerializeObject<VIFcode>(VIFCode_DIRECTHL, name: nameof(VIFCode_DIRECTHL));
                 Packets = s.SerializeObjectArrayUntil<GSTexture_Packet>(Packets, x => x.GIFTag_Packed.EOP == 1, name: nameof(Packets));
