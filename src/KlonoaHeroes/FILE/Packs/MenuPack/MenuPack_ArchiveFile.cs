6baf098d16150c7ade45226efe3d8cafcd5c2ac7
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-10-21 21:36:27 +0200

    KH: Specify the file type of MenuPack file 5

--- src/KlonoaHeroes/FILE/Packs/MenuPack/MenuPack_ArchiveFile.cs
+++ src/KlonoaHeroes/FILE/Packs/MenuPack/MenuPack_ArchiveFile.cs
@@ -7,7 +7,7 @@
         public ArchiveFile<Graphics_File> File_2 { get; set; }
         public MenuPack_File3_ArchiveFile File_3 { get; set; }
         public ArchiveFile<BestiaryEntry_Archive> Bestiary { get; set; }
-        public RawData_File File_5 { get; set; }
+        public TextCommands File_5 { get; set; }
         public Graphics_File File_6 { get; set; }
         public Graphics_File File_7 { get; set; }
         public MenuPack_File8_ArchiveFile File_8 { get; set; }
@@ -22,7 +22,7 @@
             File_2 = SerializeFile<ArchiveFile<Graphics_File>>(s, File_2, 2, name: nameof(File_2));
             File_3 = SerializeFile<MenuPack_File3_ArchiveFile>(s, File_3, 3, name: nameof(File_3));
             Bestiary = SerializeFile<ArchiveFile<BestiaryEntry_Archive>>(s, Bestiary, 4, fileEncoder: new BytePairEncoder(), name: nameof(Bestiary));
-            File_5 = SerializeFile<RawData_File>(s, File_5, 5, name: nameof(File_5));
+            File_5 = SerializeFile<TextCommands>(s, File_5, 5, name: nameof(File_5));
             File_6 = SerializeFile<Graphics_File>(s, File_6, 6, name: nameof(File_6));
             File_7 = SerializeFile<Graphics_File>(s, File_7, 7, fileEncoder: new BytePairEncoder(), name: nameof(File_7));
             File_8 = SerializeFile<MenuPack_File8_ArchiveFile>(s, File_8, 8, name: nameof(File_8));
