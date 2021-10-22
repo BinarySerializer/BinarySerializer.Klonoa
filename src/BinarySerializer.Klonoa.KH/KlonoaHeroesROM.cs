1d5d1028dd46d8e490a6118772c6805a80beba14
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-10-22 22:35:02 +0200

    KH: Read raw map data

--- src/BinarySerializer.Klonoa.KH/KlonoaHeroesROM.cs
+++ src/BinarySerializer.Klonoa.KH/KlonoaHeroesROM.cs
@@ -10,6 +10,7 @@ namespace BinarySerializer.Klonoa.KH
         public ItemsPack_Archive ItemsPack { get; set; }
         public UIPack_ArchiveFile UIPack { get; set; }
         public StoryPack_ArchiveFile StoryPack { get; set; }
+        public ArchiveFile<RawData_File> MapsPack { get; set; } // TODO: Parse maps
 
         public override void SerializeImpl(SerializerObject s)
         {
@@ -18,7 +19,6 @@ namespace BinarySerializer.Klonoa.KH
             // TODO: Move pointers to pointer table to avoid hard-coding them
 
             // TODO: Parse remaining data
-            // 08b30fd0 - KW archive with levels (each entry has 3 level ID bytes, one unknown byte and then an offset)
             // 082f9870 - offset table with names
 
             s.DoAt(new Pointer(0x08267940, Offset.File), () => MenuPack = s.SerializeObject<MenuPack_ArchiveFile>(MenuPack, name: nameof(MenuPack)));
@@ -27,6 +27,11 @@ namespace BinarySerializer.Klonoa.KH
             s.DoAt(new Pointer(0x0825c880, Offset.File), () => ItemsPack = s.SerializeObject<ItemsPack_Archive>(ItemsPack, name: nameof(ItemsPack)));
             s.DoAt(new Pointer(0x0828fd20, Offset.File), () => UIPack = s.SerializeObject<UIPack_ArchiveFile>(UIPack, name: nameof(UIPack)));
             s.DoAt(new Pointer(0x0873d350, Offset.File), () => StoryPack = s.SerializeObject<StoryPack_ArchiveFile>(StoryPack, name: nameof(StoryPack)));
+            s.DoAt(new Pointer(0x08b30fd0, Offset.File), () => MapsPack = s.SerializeObject<ArchiveFile<RawData_File>>(MapsPack, x =>
+            {
+                x.Pre_Type = ArchiveFileType.KH_KW;
+                x.Pre_ArchivedFilesEncoder = new BytePairEncoder();
+            }, name: nameof(MapsPack)));
         }
     }
 }
