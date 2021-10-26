71970a178050d73570cec2ab4119c3e3719e1e71
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-10-26 19:58:58 +0200

    KH: Mark more files as cutscenes

--- src/BinarySerializer.Klonoa.KH/FILE/Packs/StoryPack/StoryPack_ArchiveFile.cs
+++ src/BinarySerializer.Klonoa.KH/FILE/Packs/StoryPack/StoryPack_ArchiveFile.cs
@@ -12,8 +12,8 @@
         public BytePairEncoded_ArchiveFile<Graphics_File> FullscreenVignettes { get; set; }
         public Graphics_File TextBoxPrompt { get; set; }
         public ArchiveFile<ArchiveFile<Graphics_File>> CharacterPortraits_1 { get; set; }
-        public ArchiveFile<RawData_ArchiveFile> File_10 { get; set; }
-        public RawData_File File_11 { get; set; }
+        public ArchiveFile<ArchiveFile<Cutscene_File>> File_10 { get; set; }
+        public Cutscene_File File_11 { get; set; }
         public ArchiveFile<ArchiveFile<ArchiveFile<TextCommands>>> File_12 { get; set; }
         public ArchiveFile<Graphics_File> File_13 { get; set; }
         public ArchiveFile<Cutscene_File> Cutscenes_1 { get; set; }
@@ -37,8 +37,8 @@
             FullscreenVignettes = SerializeFile<BytePairEncoded_ArchiveFile<Graphics_File>>(s, FullscreenVignettes, 7, name: nameof(FullscreenVignettes));
             TextBoxPrompt = SerializeFile<Graphics_File>(s, TextBoxPrompt, 8, name: nameof(TextBoxPrompt));
             CharacterPortraits_1 = SerializeFile<ArchiveFile<ArchiveFile<Graphics_File>>>(s, CharacterPortraits_1, 9, name: nameof(CharacterPortraits_1));
-            File_10 = SerializeFile<ArchiveFile<RawData_ArchiveFile>>(s, File_10, 10, name: nameof(File_10));
-            File_11 = SerializeFile<RawData_File>(s, File_11, 11, name: nameof(File_11));
+            File_10 = SerializeFile<ArchiveFile<ArchiveFile<Cutscene_File>>>(s, File_10, 10, name: nameof(File_10));
+            File_11 = SerializeFile<Cutscene_File>(s, File_11, 11, name: nameof(File_11));
             File_12 = SerializeFile<ArchiveFile<ArchiveFile<ArchiveFile<TextCommands>>>>(s, File_12, 12, name: nameof(File_12));
             File_13 = SerializeFile<ArchiveFile<Graphics_File>>(s, File_13, 13, name: nameof(File_13));
             Cutscenes_1 = SerializeFile<ArchiveFile<Cutscene_File>>(s, Cutscenes_1, 14, name: nameof(Cutscenes_1));
