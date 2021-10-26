4f0ec57eac307599aa5af7e93d8104f00b16a8aa
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-10-26 19:22:23 +0200

    KH: A few property name changes for items

--- src/BinarySerializer.Klonoa.KH/FILE/Item/Item.cs
+++ src/BinarySerializer.Klonoa.KH/FILE/Item/Item.cs
@@ -13,8 +13,8 @@ namespace BinarySerializer.Klonoa.KH
         public ushort Ushort_06 { get; set; }
 
         public TextCommands Name { get; set; }
-        public TextCommands Description1 { get; set; }
-        public TextCommands Description2 { get; set; }
+        public TextCommands Description { get; set; }
+        public TextCommands ShopDescription { get; set; }
 
         public uint Price { get; set; }
 
@@ -24,14 +24,14 @@ namespace BinarySerializer.Klonoa.KH
         public ushort D_DF { get; set; }
         public ushort D_AG { get; set; }
 
-        public ushort A_EffectType { get; set; }
+        public ushort A_IconIndex { get; set; }
         public short A_AT { get; set; }
         public short A_SP { get; set; }
         public short A_DF { get; set; }
         public short A_AG { get; set; }
         public short A_HP { get; set; }
-        public ushort A_Unknown { get; set; }
-        public ushort A_EffectParam { get; set; }
+        public ushort A_Unknown1 { get; set; }
+        public ushort A_Unknown2 { get; set; } // Seems related to its effect
 
         public ushort I_IconIndex { get; set; }
         public ushort I_Unknown { get; set; }
@@ -55,49 +55,49 @@ namespace BinarySerializer.Klonoa.KH
                     Name = s.SerializeObject<TextCommands>(Name, x => x.Pre_MaxLength = 9, name: nameof(Name));
                     W_AT = s.Serialize<ushort>(W_AT, name: nameof(W_AT));
                     W_SP = s.Serialize<ushort>(W_SP, name: nameof(W_SP));
-                    Description1 = s.SerializeObject<TextCommands>(Description1, x => x.Pre_MaxLength = 17, name: nameof(Description1));
+                    Description = s.SerializeObject<TextCommands>(Description, x => x.Pre_MaxLength = 17, name: nameof(Description));
                     Price = s.Serialize<uint>(Price, name: nameof(Price));
-                    Description2 = s.SerializeObject<TextCommands>(Description2, x => x.Pre_MaxLength = 26, name: nameof(Description2));
+                    ShopDescription = s.SerializeObject<TextCommands>(ShopDescription, x => x.Pre_MaxLength = 26, name: nameof(ShopDescription));
                     break;
 
                 case Item_Category.D:
                     Name = s.SerializeObject<TextCommands>(Name, x => x.Pre_MaxLength = 9, name: nameof(Name));
                     D_DF = s.Serialize<ushort>(D_DF, name: nameof(D_DF));
                     D_AG = s.Serialize<ushort>(D_AG, name: nameof(D_AG));
-                    Description1 = s.SerializeObject<TextCommands>(Description1, x => x.Pre_MaxLength = 17, name: nameof(Description1));
+                    Description = s.SerializeObject<TextCommands>(Description, x => x.Pre_MaxLength = 17, name: nameof(Description));
                     Price = s.Serialize<uint>(Price, name: nameof(Price));
-                    Description2 = s.SerializeObject<TextCommands>(Description2, x => x.Pre_MaxLength = 26, name: nameof(Description2));
+                    ShopDescription = s.SerializeObject<TextCommands>(ShopDescription, x => x.Pre_MaxLength = 26, name: nameof(ShopDescription));
                     break;
 
                 case Item_Category.A:
                     Name = s.SerializeObject<TextCommands>(Name, x => x.Pre_MaxLength = 9, name: nameof(Name));
-                    A_EffectType = s.Serialize<ushort>(A_EffectType, name: nameof(A_EffectType));
+                    A_IconIndex = s.Serialize<ushort>(A_IconIndex, name: nameof(A_IconIndex));
                     A_AT = s.Serialize<short>(A_AT, name: nameof(A_AT));
                     A_SP = s.Serialize<short>(A_SP, name: nameof(A_SP));
                     A_DF = s.Serialize<short>(A_DF, name: nameof(A_DF));
                     A_AG = s.Serialize<short>(A_AG, name: nameof(A_AG));
                     A_HP = s.Serialize<short>(A_HP, name: nameof(A_HP));
-                    A_Unknown = s.Serialize<ushort>(A_Unknown, name: nameof(A_Unknown));
+                    A_Unknown1 = s.Serialize<ushort>(A_Unknown1, name: nameof(A_Unknown1));
                     Price = s.Serialize<uint>(Price, name: nameof(Price));
-                    Description1 = s.SerializeObject<TextCommands>(Description1, x => x.Pre_MaxLength = 17, name: nameof(Description1));
-                    A_EffectParam = s.Serialize<ushort>(A_EffectParam, name: nameof(A_EffectParam));
-                    Description2 = s.SerializeObject<TextCommands>(Description2, x => x.Pre_MaxLength = 24, name: nameof(Description2));
+                    Description = s.SerializeObject<TextCommands>(Description, x => x.Pre_MaxLength = 17, name: nameof(Description));
+                    A_Unknown2 = s.Serialize<ushort>(A_Unknown2, name: nameof(A_Unknown2));
+                    ShopDescription = s.SerializeObject<TextCommands>(ShopDescription, x => x.Pre_MaxLength = 24, name: nameof(ShopDescription));
                     break;
 
                 case Item_Category.I:
                     Name = s.SerializeObject<TextCommands>(Name, x => x.Pre_MaxLength = 9, name: nameof(Name));
                     I_IconIndex = s.Serialize<ushort>(I_IconIndex, name: nameof(I_IconIndex));
                     I_Unknown = s.Serialize<ushort>(I_Unknown, name: nameof(I_Unknown));
-                    Description1 = s.SerializeObject<TextCommands>(Description1, x => x.Pre_MaxLength = 17, name: nameof(Description1));
+                    Description = s.SerializeObject<TextCommands>(Description, x => x.Pre_MaxLength = 17, name: nameof(Description));
                     Price = s.Serialize<uint>(Price, name: nameof(Price));
-                    Description2 = s.SerializeObject<TextCommands>(Description2, x => x.Pre_MaxLength = 24, name: nameof(Description2));
+                    ShopDescription = s.SerializeObject<TextCommands>(ShopDescription, x => x.Pre_MaxLength = 24, name: nameof(ShopDescription));
                     break;
 
                 case Item_Category.E:
                     Name = s.SerializeObject<TextCommands>(Name, x => x.Pre_MaxLength = 9, name: nameof(Name));
                     E_IconIndex = s.Serialize<ushort>(E_IconIndex, name: nameof(E_IconIndex));
-                    Description1 = s.SerializeObject<TextCommands>(Description1, x => x.Pre_MaxLength = 20, name: nameof(Description1));
-                    Description2 = s.SerializeObject<TextCommands>(Description2, x => x.Pre_MaxLength = 24, name: nameof(Description2));
+                    Description = s.SerializeObject<TextCommands>(Description, x => x.Pre_MaxLength = 20, name: nameof(Description));
+                    ShopDescription = s.SerializeObject<TextCommands>(ShopDescription, x => x.Pre_MaxLength = 24, name: nameof(ShopDescription));
                     break;
 
                 default:
