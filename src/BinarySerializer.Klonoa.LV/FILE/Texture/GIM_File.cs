1892e2a84ba469cc64fd8f6aeb9aa5cbf5e723f4
Matthew Finerty <evilarceus@gmail.com>
2021-10-29 01:47:14 -0700

    LV: Parse GIM file

--- src/BinarySerializer.Klonoa.LV/FILE/Texture/GIM_File.cs
+++ src/BinarySerializer.Klonoa.LV/FILE/Texture/GIM_File.cs
@@ -1,9 +1,77 @@
+using BinarySerializer.PS2;
+
 namespace BinarySerializer.Klonoa.LV
 {
     // Basic 4bpp/8bpp texture format
-    // TODO: Implement this
     public class GIM_File : BaseFile
     {
-        public override void SerializeImpl(SerializerObject s) { }
+        public uint TextureCount { get; set; }
+        public ushort Texture_DBP { get; set; }
+        public ushort Texture_DBW { get; set; }
+        public GS.PixelStorageMode Texture_DPSM { get; set; }
+        public uint Texture_BPPFlag { get; set; } // Not sure what this does, but its value is 8 when the texture is 4bpp and 2 when it is 8bpp. BPP is already defined in DPSM, so might as well use that.
+        public ushort Texture_DSAX { get; set; } // X
+        public ushort Texture_DSAY { get; set; } // Y
+        public ushort Texture_RRW { get; set; } // Width
+        public ushort Texture_RRH { get; set; } // Height
+        public byte[] Texture_Data { get; set; }
+        public ushort Palette_DBP { get; set; }
+        public ushort Palette_DBW { get; set; }
+        public GS.PixelStorageMode Palette_DPSM { get; set; }
+        public uint Palette_BPPFlag { get; set; }
+        public ushort Palette_DSAX { get; set; }
+        public ushort Palette_DSAY { get; set; }
+        public ushort Palette_RRW { get; set; }
+        public ushort Palette_RRH { get; set; }
+        public PS2_RGBA8888Color[] Palette { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            // Header
+            s.SerializeMagicString("GIM", 3);
+            s.SerializePadding(1);
+            TextureCount = s.Serialize<uint>(TextureCount, name: nameof(TextureCount));
+            s.SerializePadding(8);
+
+            // Texture data
+
+            // BITBLTBUF register info
+            s.SerializeBitValues<int>(bitFunc =>
+            {
+                Texture_DBP = (ushort)bitFunc(Texture_DBP, 14, name: nameof(Texture_DBP));
+                bitFunc(default, 2, name: "Padding");
+                Texture_DBW = (ushort)bitFunc(Texture_DBW, 6, name: nameof(Texture_DBW));
+                bitFunc(default, 2, name: "Padding");
+                Texture_DPSM = (GS.PixelStorageMode)bitFunc((int)Texture_DPSM, 6, name: nameof(Texture_DPSM));
+                bitFunc(default, 2, name: "Padding");
+            });
+
+            Texture_BPPFlag = s.Serialize<uint>(Texture_BPPFlag, name: nameof(Texture_BPPFlag));
+            Texture_DSAX = s.Serialize<ushort>(Texture_DSAX, name: nameof(Texture_DSAX));
+            Texture_DSAY = s.Serialize<ushort>(Texture_DSAY, name: nameof(Texture_DSAY));
+            Texture_RRW = s.Serialize<ushort>(Texture_RRW, name: nameof(Texture_RRW));
+            Texture_RRH = s.Serialize<ushort>(Texture_RRH, name: nameof(Texture_RRH));
+            Texture_Data = s.SerializeArray<byte>(Texture_Data, Texture_DPSM == GS.PixelStorageMode.PSMT4 ? (Texture_RRW * Texture_RRH) / 2 : Texture_RRW * Texture_RRH, name: nameof(Texture_Data));
+
+            // Palette
+
+            // BITBLTBUF register info
+            s.SerializeBitValues<int>(bitFunc =>
+            {
+                Palette_DBP = (ushort)bitFunc(Palette_DBP, 14, name: nameof(Palette_DBP));
+                bitFunc(default, 2, name: "Padding");
+                Palette_DBW = (ushort)bitFunc(Palette_DBW, 6, name: nameof(Palette_DBW));
+                bitFunc(default, 2, name: "Padding");
+                Palette_DPSM = (GS.PixelStorageMode)bitFunc((int)Palette_DPSM, 6, name: nameof(Palette_DPSM));
+                bitFunc(default, 2, name: "Padding");
+            });
+
+            Palette_BPPFlag = s.Serialize<uint>(Palette_BPPFlag, name: nameof(Palette_BPPFlag));
+            Palette_DSAX = s.Serialize<ushort>(Palette_DSAX, name: nameof(Palette_DSAX));
+            Palette_DSAY = s.Serialize<ushort>(Palette_DSAY, name: nameof(Palette_DSAY));
+            Palette_RRW = s.Serialize<ushort>(Palette_RRW, name: nameof(Palette_RRW));
+            Palette_RRH = s.Serialize<ushort>(Palette_RRH, name: nameof(Palette_RRH));
+            Palette = s.SerializeObjectArray<PS2_RGBA8888Color>(Palette, Palette_RRW * Palette_RRH, name: nameof(Palette));
+        }
     }
 }
