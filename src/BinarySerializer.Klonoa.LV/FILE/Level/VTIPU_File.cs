739a1dab3f6a0016a8c60c5f1e58e2e252fc1a35
Matthew Finerty <evilarceus@gmail.com>
2021-11-05 19:33:48 -0700

    LV: Parse VTIPU file

---
+++ src/BinarySerializer.Klonoa.LV/FILE/Level/VTIPU_File.cs
@@ -0,0 +1,23 @@
+using BinarySerializer.PS2;
+
+namespace BinarySerializer.Klonoa.LV
+{
+    public class VTIPU_File : IPU_File
+    {
+        public byte[] TextureDescriptor { get; set; } // TODO: Parse this
+        public Pointer[] FramePointers { get; set; }
+
+        public override void SerializeImpl(SerializerObject s)
+        {
+            s.SerializeMagicString("vtip", 4, throwIfNoMatch: true);
+            DataSize = s.Serialize<uint>(DataSize, name: nameof(DataSize));
+            Width = s.Serialize<ushort>(Width, name: nameof(Width));
+            Height = s.Serialize<ushort>(Height, name: nameof(Height));
+            FrameCount = s.Serialize<uint>(FrameCount, name: nameof(FrameCount));
+            TextureDescriptor = s.SerializeArray<byte>(TextureDescriptor, 16, name: nameof(TextureDescriptor));
+            FramePointers = s.SerializePointerArray(FramePointers, FrameCount + 2, anchor: Offset + 0x20, name: nameof(FramePointers)); // Add 2 for the final frame delimiter and the end block
+            s.Align(16);
+            FrameData = s.SerializeArray<byte>(FrameData, DataSize, name: nameof(FrameData));
+        }
+    }
+}
