c23e1578f8b8394694c1c7a7252db81b18bb65e6
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-12 09:30:30 +0100

    DTP: Collision file count fixes

--- src/BinarySerializer.Klonoa.DTP/FILE/Model/Collision/CollisionTriangles_File.cs
+++ src/BinarySerializer.Klonoa.DTP/FILE/Model/Collision/CollisionTriangles_File.cs
@@ -10,13 +10,20 @@
         public override void SerializeImpl(SerializerObject s)
         {
             if (Pre_HasCount)
+            {
                 Count = s.Serialize<int>(Count, name: nameof(Count));
-            
-            // The count doesn't always match for some reason...
-            CollisionTriangles = s.SerializeObjectArray<CollisionTriangle>(CollisionTriangles, (Pre_FileSize - 4) / 28, name: nameof(CollisionTriangles));
 
-            if (!Pre_HasCount)
-                Count = CollisionTriangles.Length;
+                long actualCount = (Pre_FileSize - 4) / 28;
+                if (Count != actualCount)
+                    s.LogWarning($"Collision count {Count} does not match count of data in file {actualCount}");
+            }
+            else
+            {
+                Count = (int)(Pre_FileSize / 28);
+            }
+
+            // The count doesn't always match for some reason...
+            CollisionTriangles = s.SerializeObjectArray<CollisionTriangle>(CollisionTriangles, Count, name: nameof(CollisionTriangles));
         }
     }
 }
