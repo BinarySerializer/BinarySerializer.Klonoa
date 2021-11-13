75306f0e0ce65586ef104afaba1adebb11956151
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-13 16:50:16 +0100

    DTP: Name enemy waypoint values

--- src/BinarySerializer.Klonoa.DTP/CODE/LevelData2D/Enemy/Data/EnemyWaypoint.cs
+++ src/BinarySerializer.Klonoa.DTP/CODE/LevelData2D/Enemy/Data/EnemyWaypoint.cs
@@ -1,15 +1,16 @@
 ﻿namespace BinarySerializer.Klonoa.DTP
 {
+    // TODO: Rename - EnemyRange? Seems to define some range they are in on the paths (not really the movement range though).
     public class EnemyWaypoint : BinarySerializable
     {
-        public int Int_00 { get; set; }
-        public int Int_04 { get; set; }
+        public int StartPosition { get; set; }
+        public int EndPosition { get; set; }
         public short MovementPathIndex { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            Int_00 = s.Serialize<int>(Int_00, name: nameof(Int_00));
-            Int_04 = s.Serialize<int>(Int_04, name: nameof(Int_04));
+            StartPosition = s.Serialize<int>(StartPosition, name: nameof(StartPosition));
+            EndPosition = s.Serialize<int>(EndPosition, name: nameof(EndPosition));
             MovementPathIndex = s.Serialize<short>(MovementPathIndex, name: nameof(MovementPathIndex));
             s.SerializePadding(2, logIfNotNull: true);
         }
