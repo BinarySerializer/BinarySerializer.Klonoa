ff34da269bb4222d592fcb4c421a04b181a28cc1
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-19 12:36:14 +0100

    DTP: Serialize padding for empty sprite files

--- src/BinarySerializer.Klonoa.DTP/FILE/Sprite/Sprite_File.cs
+++ src/BinarySerializer.Klonoa.DTP/FILE/Sprite/Sprite_File.cs
@@ -9,6 +9,9 @@
 
         public override void SerializeImpl(SerializerObject s)
         {
+            if (Pre_FileSize == 4)
+                s.SerializePadding(4, logIfNotNull: true);
+
             Textures = s.SerializeObjectArray<SpriteTexture>(Textures, Pre_FileSize / 12, name: nameof(Textures));
         }
     }
