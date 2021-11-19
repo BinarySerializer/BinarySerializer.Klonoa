51bdf5512f8c9253b473fbf2865e541ef15b51fd
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2021-11-19 22:00:34 +0100

    DTP Proto: Correctly parse cutscene data

--- src/BinarySerializer.Klonoa.DTP/FILE/Cutscene/Cutscene.cs
+++ src/BinarySerializer.Klonoa.DTP/FILE/Cutscene/Cutscene.cs
@@ -9,19 +9,26 @@
         public Cutscene_File Cutscene_Skip { get; set; } // The game switches to this when skipping the cutscene
         public CutsceneFont_File Font { get; set; }
 
-        public RawData_File Proto_File_3 { get; set; }
-        public RawData_File Proto_File_4 { get; set; }
+        // In the final version these are all stored in the cutscene assets pack
+        public CameraAnimations_File Proto_CameraAnimations { get; set; }
+        public MovementPath_File Proto_MovementPaths { get; set; }
 
         public override void SerializeImpl(SerializerObject s)
         {
-            Cutscene_Normal = Pre_Archive.SerializeFile<Cutscene_File>(s, Cutscene_Normal, Pre_ArchiveFileIndex + 0, name: $"{nameof(Cutscene_Normal)}");
+            Loader loader = Loader.GetLoader(s.Context);
+            bool isProto = loader.GameVersion == KlonoaGameVersion.DTP_Prototype_19970717;
+
+            // Don't parse last cutscene in block 23 for prototype as it has garbage data
+            if (!(isProto && loader.BINBlock == 23 && Pre_ArchiveFileIndex == 20))
+                Cutscene_Normal = Pre_Archive.SerializeFile<Cutscene_File>(s, Cutscene_Normal, Pre_ArchiveFileIndex + 0, name: $"{nameof(Cutscene_Normal)}");
+            
             Cutscene_Skip = Pre_Archive.SerializeFile<Cutscene_File>(s, Cutscene_Skip, Pre_ArchiveFileIndex + 1, name: $"{nameof(Cutscene_Skip)}");
             Font = Pre_Archive.SerializeFile<CutsceneFont_File>(s, Font, Pre_ArchiveFileIndex + 2, name: $"{nameof(Font)}");
 
             if (Loader.GetLoader(s.Context).GameVersion == KlonoaGameVersion.DTP_Prototype_19970717)
             {
-                Proto_File_3 = Pre_Archive.SerializeFile<RawData_File>(s, Proto_File_3, Pre_ArchiveFileIndex + 3, name: $"{nameof(Proto_File_3)}");
-                Proto_File_4 = Pre_Archive.SerializeFile<RawData_File>(s, Proto_File_4, Pre_ArchiveFileIndex + 4, name: $"{nameof(Proto_File_4)}");
+                Proto_CameraAnimations = Pre_Archive.SerializeFile<CameraAnimations_File>(s, Proto_CameraAnimations, Pre_ArchiveFileIndex + 3, name: $"{nameof(Proto_CameraAnimations)}");
+                Proto_MovementPaths = Pre_Archive.SerializeFile<MovementPath_File>(s, Proto_MovementPaths, Pre_ArchiveFileIndex + 4, name: $"{nameof(Proto_MovementPaths)}");
             }
         }
     }
