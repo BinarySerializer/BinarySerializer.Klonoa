62a530fd777fd7d8f27c5cb1c534fceea9ac976a
entriphy <1747088+entriphy@users.noreply.github.com>
2022-12-24 17:00:43 -0800

    LV: Fix VIFGeometry parsing for Type 1 files

--- src/BinarySerializer.Klonoa.LV/FILE/Geometry/VIFGeometry_File.cs
+++ src/BinarySerializer.Klonoa.LV/FILE/Geometry/VIFGeometry_File.cs
@@ -2,6 +2,11 @@ namespace BinarySerializer.Klonoa.LV
 {
     public class VIFGeometry_File : BaseFile
     {
+        /// <summary>
+        /// Decides how to parse this file.
+        /// If 0: SectionCount is multiplied by 2 when parsing sections.
+        /// If 1: SectionCount is equal to one.
+        /// </summary>
         public uint Type;
         public uint SectionCount;
         public VIFGeometry_Section[] Sections;
@@ -9,9 +14,14 @@ namespace BinarySerializer.Klonoa.LV
         public override void SerializeImpl(SerializerObject s)
         {
             Type = s.Serialize<uint>(Type, name: nameof(Type));
-            SectionCount = s.Serialize<uint>(SectionCount, name: nameof(SectionCount));
+            if (Type == 0) {
+                SectionCount = s.Serialize<uint>(SectionCount, name: nameof(SectionCount));
+            } else {
+                SectionCount = 1;
+                s.SerializePadding(4, logIfNotNull: true);
+            }
             s.SerializePadding(8);
-            Sections = s.SerializeObjectArray<VIFGeometry_Section>(Sections, SectionCount * 2, name: nameof(Sections), onPreSerialize: x => x.Pre_Offset = Offset);
+            Sections = s.SerializeObjectArray<VIFGeometry_Section>(Sections, SectionCount * (Type == 0 ? 2 : 1), name: nameof(Sections), onPreSerialize: x => x.Pre_Offset = Offset);
         }
     }
 }
